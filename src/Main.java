c1d62b430f11c8a989e376fcdaff8660e0104879
mukabyl <mukabyl@yamdex.ru>
2023-02-19 21:42:07 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        User user = new User(10L, "meder", "tynychbekov", "meder@gmail.com", 23);
+        User user1 = new User(23L, "asan", "tairov", "asan@gmail.com", 19);
+        User user2 = new User(3L, "uson", "kamylov", "uson@gmail.com", 20);
+        User user3 = new User(4L, "Bakyt", "Latibov", "bakyt@gmail.com", 22);
+
+        List<User> users = new ArrayList<>(Arrays.asList(user, user1, user2, user3));
+
+        users.forEach(System.out::println);
+
+        System.out.println("------------------------------");
+
+        Collections.sort(users, user.compareById);
+
+        users.forEach(System.out::println);
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,85 @@
-package PACKAGE_NAME;public class User {
+import java.util.Comparator;
+
+//public class User implements Comparable<User>
+public class User {
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+    private int age;
+
+    public User(Long id, String name, String lastname, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    //    @Override
+//    public int compareTo(User o) {
+//        return id.compareTo(o.id);
+//    }
+    Comparator<User> compareById = new Comparator<User>() {
+        @Override
+        public int compare(User o1, User o2) {
+            return o1.id.compareTo(o2.id);
+        }
+    };
+    Comparator<User> compareByAge = new Comparator<User>() {
+        @Override
+        public int compare(User o1, User o2) {
+            return o1.age - o2.age;
+        }
+    };
 }
